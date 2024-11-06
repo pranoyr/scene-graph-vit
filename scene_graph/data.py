0366562223af0592d9f3ab79d67e492ad594b612
Pranoy <pranoy@Pranoys-MacBook-Air.local>
2024-11-06 14:51:00 +0530

    trainer for obj

---
+++ scene_graph/data.py
@@ -0,0 +1,222 @@
+import json
+import os
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import torch
+from PIL import Image
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms, utils
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def y1y2x1x2_to_x1y1x2y2(y1y2x1x2):
+	x1 = y1y2x1x2[2]
+	y1 = y1y2x1x2[0]
+	x2 = y1y2x1x2[3]
+	y2 = y1y2x1x2[1]
+	return [x1, y1, x2, y2]
+
+
+def make_image_list(dataset_path, type):
+	imgs_list = []
+	with open(os.path.join(dataset_path, 'json_dataset', f'annotations_{type}.json'), 'r') as f:
+		annotations = json.load(f)
+	sg_images = os.listdir(os.path.join(
+		dataset_path, f'sg_{type}_images'))
+
+	annotations_copy = annotations.copy()
+	for ann in annotations.items():
+		if(not annotations[ann[0]] or ann[0] not in sg_images):
+			annotations_copy.pop(ann[0])
+
+	for ann in annotations_copy.items():
+		imgs_list.append(ann[0])
+	return imgs_list
+
+
+class VRDDataset(Dataset):
+	"""VRD dataset."""
+
+	def __init__(self, dataset_path, image_set):
+		self.dataset_path = dataset_path
+		self.image_set = image_set
+		# read annotations file
+		with open(os.path.join(self.dataset_path, 'json_dataset', f'annotations_{self.image_set}.json'), 'r') as f:
+			self.annotations = json.load(f)
+		with open(os.path.join(self.dataset_path, 'json_dataset', 'objects.json'), 'r') as f:
+			self.all_objects = json.load(f)
+		with open(os.path.join(self.dataset_path, 'json_dataset', 'predicates.json'), 'r') as f:
+			self.predicates = json.load(f)
+
+		self.root = os.path.join(
+			self.dataset_path, f'sg_{self.image_set}_images')
+
+		self.classes = self.all_objects.copy()
+		self.preds = self.predicates.copy()
+		# self.classes.insert(0, '__background__')
+		self.preds.insert(0, 'unknown')
+
+		self._class_to_ind = dict(zip(self.classes, range(len(self.classes))))
+		self.ind_to_class = dict(zip(range(len(self.classes)), self.classes))
+		self._preds_to_ind = dict(
+			zip(self.preds, range(len(self.preds))))
+		self.imgs_list = make_image_list(self.dataset_path, self.image_set)
+
+		self.transform = transforms.Compose([
+			transforms.Resize((256, 256)),
+			transforms.ToTensor(),
+			transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+		])
+
+	def __len__(self):
+		return len(self.imgs_list)
+
+	def image_path_from_index(self, img_name):
+		"""
+		Construct an image path from the image's "index" identifier.
+		"""
+		image_path = os.path.join(self.dataset_path, f'sg_{self.image_set}_images',
+								  img_name)
+		assert os.path.exists(image_path), \
+			'Path does not exist: {}'.format(image_path)
+		return image_path
+
+	def transform_boxes(self, boxes, original_size):
+		"""
+		Transform the bounding boxes to the scale of 224x224 image.
+		"""
+
+		orig_w, orig_h = original_size
+		scale_w = 256 / orig_w
+		scale_h = 256 / orig_h
+		transformed_boxes = []
+	
+		for box in boxes:
+			x1, y1, x2, y2 = box
+			x1 = int(x1 * scale_w)
+			y1 = int(y1 * scale_h)
+			x2 = int(x2 * scale_w)
+			y2 = int(y2 * scale_h)
+			transformed_boxes.append([x1, y1, x2, y2])
+		return transformed_boxes
+
+	def load_pascal_annotation(self, index, original_size):
+		"""
+		Load image and bounding boxes info from XML file in the PASCAL VOC
+		format.
+		"""
+		boxes = []
+		labels = []
+		preds = []
+		# preds = []
+		annotation = self.annotations[index]
+		for spo in annotation:
+			gt_sbj_label = spo['subject']['category']
+			gt_sbj_bbox = spo['subject']['bbox']
+			gt_obj_label = spo['object']['category']
+			gt_obj_bbox = spo['object']['bbox']
+			predicate = spo['predicate']
+
+			# prepare bboxes for subject and object
+			gt_sbj_bbox = y1y2x1x2_to_x1y1x2y2(gt_sbj_bbox)
+			gt_obj_bbox = y1y2x1x2_to_x1y1x2y2(gt_obj_bbox)
+			# transform the boxes
+			gt_sbj_bbox = self.transform_boxes([gt_sbj_bbox], original_size)[0]
+			gt_obj_bbox = self.transform_boxes([gt_obj_bbox], original_size)[0]
+			boxes.append([gt_sbj_bbox, gt_obj_bbox])
+
+			# prepare labels for subject and object
+			# map to word
+			gt_sbj_label = self.all_objects[gt_sbj_label]
+			gt_obj_label = self.all_objects[gt_obj_label]
+			predicate = self.predicates[predicate]
+			# map to new index
+			labels.append([self._class_to_ind[gt_sbj_label],
+						   self._class_to_ind[gt_obj_label]])
+			preds.append(self._preds_to_ind[predicate])
+		return boxes, labels, preds
+
+	def __getitem__(self, index):
+		img_name = self.imgs_list[index]
+		img_path = self.image_path_from_index(img_name)
+		img = Image.open(img_path)
+		original_size = img.size
+		boxes, labels, preds = self.load_pascal_annotation(img_name, original_size)
+		img = self.transform(img)
+
+		
+		assert len(boxes) == len(
+			labels), "boxes and labels should be of equal length"
+
+		return {'boxes': torch.tensor(boxes, dtype=torch.float32),
+				'labels': torch.tensor(labels, dtype=torch.int64),
+				'preds': torch.tensor(preds, dtype=torch.int64),
+				'img': img
+				}
+	
+
+def collater(data):
+	imgs = [s['img'] for s in data]
+	imgs = torch.stack(imgs, 0)
+	annotations = [{"boxes": s['boxes'].to(device)} for s in data]
+	for i, s in enumerate(data):
+		annotations[i]['labels'] = s['labels'].to(device)
+	for i, s in enumerate(data):
+			annotations[i]['preds'] = s['preds'].to(device)
+	return imgs, annotations
+
+
+
+def build_dataset(cfg):
+	train_dataset = VRDDataset(cfg.dataset.params.root_path, "train")
+	val_dataset = VRDDataset(cfg.dataset.params.root_path, "test")
+	train_loader = DataLoader(train_dataset, batch_size=cfg.dataset.params.batch_size,
+							  shuffle=cfg.dataset.params.shuffle, collate_fn=collater)
+	val_loader = DataLoader(val_dataset, batch_size=cfg.dataset.params.batch_size,
+							shuffle=False, collate_fn=collater)
+	
+	return train_loader, val_loader
+
+
+if __name__ == '__main__':
+	dataset = VRDDataset("/Users/pranoy/Downloads/vrd", "train")
+	dataloader = DataLoader(dataset, batch_size=2, shuffle=True, collate_fn=collater)
+	for i, data in enumerate(dataloader):
+		imgs, annotations = data
+		# print(annotations)
+
+
+		# visualize the image and the bounding boxes
+		for j in range(len(imgs)):
+			img = imgs[j].permute(1, 2, 0).cpu().numpy()
+			img = img * np.array([0.229, 0.224, 0.225]) + np.array([0.485, 0.456, 0.406])
+			img = np.clip(img, 0, 1)
+
+			plt.figure(figsize=(10, 10))
+			plt.imshow(img)
+
+			for boxes, labels in zip(annotations[j]['boxes'], annotations[j]['labels']):
+				for box, label in zip(boxes, labels):
+					x1, y1, x2, y2 = box
+					plt.text(x1, y1, dataset.ind_to_class[label.item()], fontsize=12, color='r')
+					plt.plot([x1, x2, x2, x1, x1], [y1, y1, y2, y2, y1], color='r')
+				
+			
+			plt.show()
+	
+
+		break
+		# print(imgs)
+		# print(annotations)
+		# print(annotations['boxes'])
+		# print(annotations['labels'])
+		# print(annotations['preds'])
+		# print(annotations['img'])
+		# print(annotations['img'].shape)
+		# print(annotations['boxes'].shape)
+		# print(annotations['labels'].shape)
+		# print(annotations['preds
---
+++ scene_graph/trainer.py
@@ -0,0 +1,249 @@
+import os
+import torch
+import random
+import math
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+from accelerate import Accelerator
+from torch.utils.data import DataLoader, random_split
+from tqdm.auto import tqdm
+from einops import rearrange
+import logging
+import torch.nn as nn
+from transformers import get_constant_schedule_with_warmup, get_cosine_schedule_with_warmup
+import torch
+
+
+
+def get_scheduler(cfg, optimizer , **kwargs):
+    warmup_steps = cfg.lr_scheduler.params.warmup_steps
+    
+    if cfg.lr_scheduler.name == "constant_with_warmup":
+        scheduler = get_constant_schedule_with_warmup(optimizer, num_warmup_steps=warmup_steps)
+    elif cfg.lr_scheduler.name == "cosine_with_warmup":
+        scheduler = get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=warmup_steps, num_training_steps=kwargs["decay_steps"])
+
+    return scheduler
+
+
+
+def get_optimizer(cfg, params):
+    lr = cfg.optimizer.params.learning_rate
+    warmup_steps = cfg.lr_scheduler.params.warmup_steps
+    beta1 = cfg.optimizer.params.beta1
+    beta2 = cfg.optimizer.params.beta2
+    decay_steps = cfg.lr_scheduler.params.decay_steps
+    weight_decay = cfg.optimizer.params.weight_decay
+    
+    if cfg.optimizer.name == "adamw":
+        optimizer = torch.optim.AdamW(params, lr=lr, betas=(beta1, beta2), weight_decay=weight_decay)
+    elif cfg.optimizer.name == "adam":
+        optimizer = torch.optim.Adam(params, lr=lr, betas=(beta1, beta2), weight_decay=weight_decay)
+
+    return optimizer
+
+
+
+class BaseTrainer(object):
+	def __init__(
+		self, 
+		cfg,
+		model,
+		dataloaders
+		):
+
+		self.cfg = cfg
+		self.project_name = cfg.experiment.project_name
+		self.exp_name = cfg.experiment.exp_name
+		
+		# init accelerator
+		self.accelerator = Accelerator(
+			mixed_precision=cfg.training.mixed_precision,
+			gradient_accumulation_steps=cfg.training.gradient_accumulation_steps,
+			log_with="wandb"
+		)
+		self.accelerator.init_trackers(
+				project_name=cfg.experiment.project_name,
+				init_kwargs={"wandb": {
+				"config" : cfg,
+				"name" : self.exp_name}
+		})
+
+		# models and dataloaders
+		self.model = model
+		self.train_dl , self.val_dl = dataloaders
+		self.global_step = 0
+		self.num_epoch = cfg.training.num_epochs
+		self.gradient_accumulation_steps = cfg.training.gradient_accumulation_steps
+		self.batch_size = cfg.dataset.params.batch_size
+		self.max_grad_norm = cfg.training.max_grad_norm
+
+		# logging details
+		self.num_epoch = cfg.training.num_epochs
+		self.save_every = cfg.experiment.save_every
+		self.sample_every = cfg.experiment.sample_every
+		self.log_every = cfg.experiment.log_every
+		self.eval_every = cfg.experiment.eval_every
+		
+		# Resume from ckpt
+		if cfg.experiment.resume_path_from_checkpoint:
+			path = cfg.experiment.resume_path_from_checkpoint
+			self.resume_from_checkpoint(path)
+	
+	
+		# Checkpoint and generated images folder
+		output_folder = f"outputs/{cfg.experiment.project_name}"
+		self.checkpoint_folder = os.path.join(output_folder, 'checkpoints')
+		os.makedirs(self.checkpoint_folder, exist_ok=True)
+		
+		self.image_saved_dir = os.path.join(output_folder, 'images')
+		os.makedirs(self.image_saved_dir, exist_ok=True)
+
+
+		logging.info(f"Train dataset size: {len(self.train_dl.dataset)}")
+		logging.info(f"Val dataset size: {len(self.val_dl.dataset)}")
+
+		# effective iteration considering gradient accumulation
+		effective_batch_size = self.batch_size * self.gradient_accumulation_steps
+		self.num_iters_per_epoch = math.ceil(len(self.train_dl.dataset) / effective_batch_size)
+		self.total_iters = self.num_epoch * self.num_iters_per_epoch
+		logging.info(f"Number of iterations per epoch: {self.num_iters_per_epoch}")
+		logging.info(f"Total training iterations: {self.total_iters}")
+
+		
+	@property
+	def device(self):
+		return self.accelerator.device
+	
+	
+	def train(self):
+		raise NotImplementedError("Train method not implemented")
+		
+
+	def save_ckpt(self, rewrite=False):
+		"""Save checkpoint"""
+
+		filename = os.path.join(self.checkpoint_folder, f'{self.project_name}_{self.exp_name}_step_{self.global_step}.pt')
+		if rewrite:
+			filename = os.path.join(self.checkpoint_folder, f'{self.project_name}_{self.exp_name}.pt')
+		
+		checkpoint={
+				'step': self.global_step,
+				'state_dict': self.accelerator.unwrap_model(self.model).state_dict(),
+				'config': self.cfg
+
+			}
+
+		self.accelerator.save(checkpoint, filename)
+		logging.info("Saving checkpoint: %s ...", filename)
+   
+   
+	def resume_from_checkpoint(self, checkpoint_path):
+		"""Resume from checkpoint"""
+		checkpoint = torch.load(checkpoint_path)
+		self.global_step = checkpoint['step']
+		self.model.load_state_dict(checkpoint['state_dict'])
+		logging.info("Resume from checkpoint %s (global_step %d)", checkpoint_path, self.global_step)
+
+
+	@torch.no_grad()
+	def evaluate(self):
+		raise NotImplementedError("Evaluate method not implemented")
+	
+
+
+
+class SceneGraphTrainer(BaseTrainer):
+	def __init__(
+		self, 
+		cfg,
+		model,
+		dataloaders
+		):
+		super().__init__(cfg, model, dataloaders)
+  
+		# Training parameters
+		decay_steps = cfg.lr_scheduler.params.decay_steps
+
+  
+		if not decay_steps:
+			decay_steps = self.num_epoch * self.num_iters_per_epoch
+
+
+		params = self.model.parameters()
+		self.optim = get_optimizer(cfg, params)
+		self.scheduler = get_scheduler(cfg, self.optim, decay_steps=decay_steps)
+
+		(
+			self.model,
+			self.optim,
+			self.scheduler,
+			self.train_dl
+	
+		) = self.accelerator.prepare(
+			self.model,
+			self.optim,
+			self.scheduler,
+			self.train_dl
+	 )
+		
+		# logging details
+		self.num_epoch = cfg.training.num_epochs
+		self.save_every = cfg.experiment.save_every
+		self.sample_every = cfg.experiment.sample_every
+		self.eval_every = cfg.experiment.eval_every
+		self.log_every = cfg.experiment.log_every
+		self.max_grad_norm = cfg.training.max_grad_norm
+		
+  
+	def train(self):
+		start_epoch=self.global_step//len(self.train_dl)
+		self.model.train()
+		for epoch in range(start_epoch, self.num_epoch):
+			with tqdm(self.train_dl, dynamic_ncols=True, disable=not self.accelerator.is_main_process) as train_dl:
+				for batch in train_dl:
+					img , annots = batch
+					img = img.to(self.device)
+
+					with self.accelerator.accumulate(self.model):
+						with self.accelerator.autocast():
+							loss_dict = self.model(img, annots)
+							weight_dict = self.model.criterion.weight_dict
+							losses = sum(loss_dict[k] * weight_dict[k] for k in loss_dict.keys() if k in weight_dict)
+
+							self.accelerator.backward(losses)
+							if self.accelerator.sync_gradients and self.max_grad_norm:
+								self.accelerator.clip_grad_norm_(self.model.parameters(), self.max_grad_norm)
+							self.optim.step()
+							self.scheduler.step(self.global_step)
+							self.optim.zero_grad()
+							
+							
+						if not (self.global_step % self.save_every):
+							self.save_ckpt(rewrite=True)
+						
+						# if not (self.global_step % self.eval_every):
+						# 	self.model.eval()
+						# 	outputs = torch.softmax(outputs, dim=1)
+						# 	acc = (outputs.argmax(dim=1) == target).float().mean().item()
+						# 	self.accelerator.log({"acc": acc}, step=self.global_step)
+						# 	self.evaluate()
+						# 	self.model.train()
+		
+						if not (self.global_step % self.gradient_accumulation_steps):
+							lr = self.optim.param_groups[0]['lr']
+							self.accelerator.log({"loss": losses.item(), "lr": lr}, step=self.global_step)
+				
+						self.global_step += 1
+	  
+					
+		self.accelerator.end_training()        
+		print("Train finished!")
+  
+
+
+
+
+
+
